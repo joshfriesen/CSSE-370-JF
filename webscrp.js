cd707f2f1ac28ad6ec5f4f003ea2feab1c8c7a64
joshfriesen <35316025+joshfriesen@users.noreply.github.com>
2020-04-03 15:49:47 -0700

    Update webscrp.js

--- webscrp.js
+++ webscrp.js
@@ -1,3 +1,12 @@
+window.onload=init;
+
+async function init(){
+	const api_url='http://api.openweathermap.org/data/2.5/weather?q=Fresno,us&APPID=87e3c3bb474cc0be0e7747a2d9d2f1fb&units=imperial';
+        const response = await fetch(api_url);
+        const data = await response.json();
+	document.getElementById("dd").innerHTML = "Fresno temp is " + data.main.temp + "°F click for today's max temp";
+}
+	
 
 function funfun() {
     
@@ -11,6 +20,7 @@ function notnot() {
 function getTime() {
   document.getElementById("time").innerHTML = Date();
     document.getElementById("time").style.display='block';
+
 }
 
 function invisible(id){
@@ -20,4 +30,11 @@ function invisible(id){
     else 
         temp.style.display='block';
     temp.style.color='red';
+}
+
+async function getData(){
+    const api_url='http://api.openweathermap.org/data/2.5/weather?q=Fresno,us&APPID=87e3c3bb474cc0be0e7747a2d9d2f1fb&units=imperial';
+    const response = await fetch(api_url);
+    const data = await response.json();
+    document.getElementById("maxT").innerHTML = data.main.temp_max + "°F";
 }
